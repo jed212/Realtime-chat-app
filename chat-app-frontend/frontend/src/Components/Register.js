47e3524347e3ac9d868bdce1fc44dff1225719b8
jedidiah <jedidahfavour@gmail.com>
2023-10-26 18:15:07 +0300

    add changes

---
+++ chat-app-frontend/frontend/src/Components/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {TextField} from '@mui/material';
+
+export default function Register() {
+  return (
+    <>
+      <div className='container'>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </div>
+    </>
+  );
+}
