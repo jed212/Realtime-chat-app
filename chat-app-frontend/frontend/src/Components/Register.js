ec284e1a10f6bbe026e2eea4289d7c031515f268
jedidiah <jedidahfavour@gmail.com>
2023-10-29 15:27:22 +0300

    configure register

--- chat-app-frontend/frontend/src/Components/Register.js
+++ chat-app-frontend/frontend/src/Components/Register.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import {TextField} from '@mui/material';
+import ReactDOM from 'react-dom'
+import {TextField, Button} from '@mui/material';
 
 export default function Register() {
   return (
     <>
-      <div className='container'>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <div className='container text-center'>
+        <div className='mt-3'>
+          <TextField id="email" type="email" label="Email" variant="outlined" />
+        </div>
+        <div className='mt-3'>
+        <TextField id="first_name" type="text" label="First Name" variant="outlined" />
+        </div>
+        <div className='mt-3'>
+        <TextField id="last_name" type="text" label="Last Name" variant="outlined" />
+        </div>
+        <div className='mt-3'>
+        <TextField id="password" type="password" label="Password" variant="outlined" />
+        </div>
+        <div className='mt-3'>
+        <Button variant='contained'>Save</Button>
+        </div>
       </div>
     </>
   );
