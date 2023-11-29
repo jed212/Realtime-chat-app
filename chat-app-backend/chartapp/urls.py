6845c5f8233dd296a41b5222fbdf2dabb8ff16a8
jedidiah <jedidahfavour@gmail.com>
2023-11-29 19:43:50 +0300

    create users endpoint for users list

--- chat-app-backend/chartapp/urls.py
+++ chat-app-backend/chartapp/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from accounts.views import register_user, login
+from chat.views import get_user_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',register_user, name="register"),
-    path('login/',login, name="login")
+    path('login/',login, name="login"),
+    path('api/users/', get_user_list, name="users")
 ]
